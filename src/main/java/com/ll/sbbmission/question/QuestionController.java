b4ad04e4fe14212619ce35bd5263cff2eaba6bc6
kds98 <skyudae@naver.com>
2023-11-25 15:11:27 +0900

    2-15 question create / form

--- src/main/java/com/ll/sbbmission/question/QuestionController.java
+++ src/main/java/com/ll/sbbmission/question/QuestionController.java
@@ -1,10 +1,14 @@
 package com.ll.sbbmission.question;
 
+import com.ll.sbbmission.answer.AnswerForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,6 +18,7 @@ import java.util.List;
 @RequestMapping("/question")
 public class QuestionController {
     private final QuestionService questionService;
+
     @GetMapping("/list")
     String list(Model model) {
         List<Question> questionList = this.questionService.getList();
@@ -23,10 +28,25 @@ public class QuestionController {
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
 
         return "question_detail";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
+    }
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm){
+        return "question_form";
     }
 }
